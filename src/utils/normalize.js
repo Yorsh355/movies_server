c875b9ff8030d32124ec8b716796fb486e60e93e
Yorman Londono <yorsh355@gmail.com>
2023-05-20 17:59:57 -0500

    Server totalmente funcional

--- src/utils/normalize.js
+++ src/utils/normalize.js
@@ -2,7 +2,8 @@ const normalizeString = (str) => {
   return str
     .toLowerCase()
     .normalize("NFD")
-    .replace(/[\u0300-\u036f]/g, "");
+    .replace(/[\u0300-\u036f]/g, "")
+    .trim();
 };
 
 module.exports = {
