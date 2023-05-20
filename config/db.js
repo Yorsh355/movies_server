eee87a108a0af95f9cba554ba4592f5301e77882
Yorman Londono <yorsh355@gmail.com>
2023-05-20 15:37:30 -0500

    Deploy

--- config/db.js
+++ config/db.js
@@ -1,14 +1,19 @@
 require("dotenv").config({ path: "../.env" });
 const { Pool } = require("pg");
 
-const { PGUSER, PGHOST, PGPASSWORD, PGDATABASE, PGPORT } = process.env;
+const { PGUSER, PGHOST, PGPASSWORD, PGDATABASE, PGPORT, DB_DEPLOY } =
+  process.env;
 
-const pool = new Pool({
+/* const pool = new Pool({
   user: PGUSER,
   host: PGHOST,
   password: PGPASSWORD,
   database: PGDATABASE,
   port: PGPORT,
+}); */
+
+const pool = new Pool({
+  DB_DEPLOY,
 });
 
 module.exports = pool;
